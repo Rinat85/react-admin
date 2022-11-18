cf9d95e608444de5d8b50d3dbfad495b1ca8f94a
Rinat85 <pacmanreal@gmail.com>
2022-11-18 18:40:41 +0500

    feat: add sidebar and topbar, start themeing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import { ColorModeContext, useMode } from "./theme";
+import { Routes, Route } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import { ProSidebarProvider } from 'react-pro-sidebar';
 import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+// import Team from "./scenes/team";
+// import Invoices from "./scenes/invoices";
+// import Contacts from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+// import Geography from "./scenes/geography";
+// import Calendar from "./scenes/calendar";
 
 function App() {
 
@@ -11,9 +25,25 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <main className="content">
-            <Topbar />
-          </main>
+          <ProSidebarProvider>
+            <Sidebar />
+            <main className="content">
+              <Topbar />
+              <Routes>
+                <Route path="/" element={<Dashboard />}/>
+                {/* <Route path="/team" element={<Team />}/> */}
+                {/* <Route path="/invoices" element={<Invoices />}/> */}
+                {/* <Route path="/contacts" element={<Contacts />}/> */}
+                {/* <Route path="/bar" element={<Bar />}/> */}
+                {/* <Route path="/form" element={<Form />}/> */}
+                {/* <Route path="/line" element={<Line />}/> */}
+                {/* <Route path="/pie" element={<Pie />}/> */}
+                {/* <Route path="/faq" element={<FAQ />}/> */}
+                {/* <Route path="/geography" element={<Geography />}/> */}
+                {/* <Route path="/calendar" element={<Calendar />}/> */}
+              </Routes>
+            </main>
+          </ProSidebarProvider>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
